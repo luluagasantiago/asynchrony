6912f57f1a19678a467db30ea8db21d894d0b1bc
luluagasantiago <luluagasantiago@icloud.com>
2023-11-10 23:40:31 +0100

    change of API

--- summer.py
+++ summer.py
@@ -1,29 +1,27 @@
-import json
 import time
 import asyncio
 
 
 import aiohttp
 
+
 async def worker(name, n, session):
     """make requests on  qrng API . get N random numbers"""
     print(f"worker: {name}")
-    url = f"http://qrng.anu.edu.au/API/jsonI.php?length={n}type=uint16"
+    url = f"https://api.quantumnumbers.anu.edu.au"
+    params = {'length': n, 'type': 'uint16', 'size': '1'}
+    headers = {'x-api-key': 'Z355gDqnFb3NMYytkSGzF4qNNaHtIbTB5raEoo3a'}
     # await because we'll do asynchronous io
-    response = await session.request(method = 'GET', url = url)
-    value = await response.text() 
-    if 'limited' in value:
-        return value
-    else:
-        value = json.loads(value)
-        return sum(value['data'])
-    
-
+    response = await session.get(url, params = params, headers= headers)
+    if response.status == 200:
+        data = await response.json()
+        return sum(data['data'])
+    return 0
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        response = await worker('bob', 3, session)
-        print("response: ", response)
+        sums = await asyncio.gather(*(worker(f'w{i}', n, session) for i, n in enumerate(range(2,5))))
+        print("sums: ", sums)
 
 
 if __name__ == "__main__": 
