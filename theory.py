e9c401f3295d7a63faad68c0057b2d82eee9845d
luluagasantiago <luluagasantiago@icloud.com>
2023-11-10 21:42:04 +0100

    event loop added and multiple task testing in main()

--- theory.py
+++ theory.py
@@ -5,7 +5,7 @@ import asyncio
 
 # Generators :(Producer) sequence of any kind of values
 
-def odds(start: int , stop: int):
+def odds(start: int , stop: int) -> int:
     """Generator inclusive sequence"""
     for odd in range(start, stop + 1, 2):
         #yiels: return out value and then pause
@@ -16,19 +16,36 @@ def odds(start: int , stop: int):
 # Corouritines: (Consumer) Consume values
 
 # In order to use await, we need the function to be async
-async def randn():
+async def randn() -> int : 
     #await allows this particular function to pause
     # But other things can continue to run. 
     # We could imagine this as "running in the background"
     await asyncio.sleep(3)
     return randint(1,10)
 
-def main():
+async def main():
     odd_values = [odd for odd in odds(3,15)]
     print(odd_values)
     odds2 = tuple(odds(21,29))
     print(odds2)
 
+    start = time.perf_counter()
+    r = await randn()
+    elapsed =time.perf_counter() - start
+    
+    print(f"{r} took {elapsed:0.2f} seconds.")
 
+    start = time.perf_counter()
+    # We gather more than one task to test the behaviour of the code 
+    # wehn using asyncio tasks and the event loop
+    # expected behavior: all the taks (randn) should take only 3 seconds
+    r = await  asyncio.gather(*(randn() for _ in range(10)))
+    elapsed =time.perf_counter() - start
+    
+    print(f"{r} took {elapsed:0.2f} seconds.")
+
+
+
+#event loop: run async tasks, perform IO ops and suprocesses.
 if __name__ == "__main__":
-    main()
+    asyncio.run(main()) 
