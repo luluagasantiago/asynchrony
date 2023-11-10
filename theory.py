ea26fb519678868f1c7b6c39c2a77735096e8191
luluagasantiago <luluagasantiago@icloud.com>
2023-11-10 15:51:37 +0100

    async rand coroutine added

--- theory.py
+++ theory.py
@@ -1,4 +1,9 @@
-# Generators : sequence of any kind of values
+from random import randint
+import time 
+import asyncio
+
+
+# Generators :(Producer) sequence of any kind of values
 
 def odds(start: int , stop: int):
     """Generator inclusive sequence"""
@@ -6,3 +11,21 @@ def odds(start: int , stop: int):
         #yiels: return out value and then pause
         # with next(), ask the function to resume
         yield odd
+
+
+# Corouritines: (Consumer) Consume values
+
+#sync version
+def randn():
+    time.sleep(3)
+    return randint(1,10)
+
+def main():
+    odd_values = [odd for odd in odds(3,15)]
+    print(odd_values)
+    odds2 = tuple(odds(21,29))
+    print(odds2)
+
+
+if __name__ == "__main__":
+    main()
