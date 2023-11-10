098f36a5b3f015315cd6e01598088436d02e78f3
luluagasantiago <luluagasantiago@icloud.com>
2023-11-10 22:05:02 +0100

    square_odds added to demonstrate looping through something that's async

--- theory.py
+++ theory.py
@@ -5,7 +5,7 @@ import asyncio
 
 # Generators :(Producer) sequence of any kind of values
 
-def odds(start: int , stop: int) -> int:
+def odds(start: int , stop: int):
     """Generator inclusive sequence"""
     for odd in range(start, stop + 1, 2):
         #yiels: return out value and then pause
@@ -16,12 +16,19 @@ def odds(start: int , stop: int) -> int:
 # Corouritines: (Consumer) Consume values
 
 # In order to use await, we need the function to be async
-async def randn() -> int : 
+async def randn() : 
     #await allows this particular function to pause
     # But other things can continue to run. 
     # We could imagine this as "running in the background"
     await asyncio.sleep(3)
     return randint(1,10)
+
+async def square_odds(start: int, stop: int):
+    for odd in odds(start, stop):
+        await asyncio.sleep(2) # suppose we're waiting for a webserver
+        yield odd ** 2
+
+
 
 async def main():
     odd_values = [odd for odd in odds(3,15)]
@@ -44,6 +51,9 @@ async def main():
     
     print(f"{r} took {elapsed:0.2f} seconds.")
 
+    # looping for something that's async
+    async for so in square_odds(11, 17):
+        print("so: ", so)
 
 
 #event loop: run async tasks, perform IO ops and suprocesses.
